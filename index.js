1460bac98ba317034748936650794aec7cedd8fa
AC59852 <austin.caron1@gmail.com>
2023-01-18 20:32:41 -0500

    attempting update for render

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 const express = require('express');
 const axios = require('axios');
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 // const oauthRoute = 'https://oauth.battle.net/token';
 
